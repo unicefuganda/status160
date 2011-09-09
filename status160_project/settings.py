c9011f4cc89e5fe7bd71dcad86af183436744a27
David McCann <david.a.mccann@gmail.com>
2011-09-09 10:50:09 +0300

    removed authsites refs

--- status160_project/settings.py
+++ status160_project/settings.py
@@ -12,7 +12,6 @@ filedir = os.path.dirname(__file__)
 sys.path.append(os.path.join(filedir))
 sys.path.append(os.path.join(filedir, 'rapidsms', 'lib'))
 sys.path.append(os.path.join(filedir, 'rapidsms_auth'))
-sys.path.append(os.path.join(filedir, 'rapidsms_authsites'))
 sys.path.append(os.path.join(filedir, 'rapidsms_contact'))
 sys.path.append(os.path.join(filedir, 'rapidsms_generic'))
 sys.path.append(os.path.join(filedir, 'rapidsms_httprouter_src'))
@@ -86,7 +85,6 @@ INSTALLED_APPS = [
     "rapidsms.contrib.registration",
     "rapidsms.contrib.locations",
     "rapidsms.contrib.locations.nested",
-    "authsites",
     "eav",
     "auth",
     "rapidsms_httprouter",
